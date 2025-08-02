866b14b788ddc2aeb1b9c3f5f3f309fde6da580f
anshika-029 <167740156+anshika-029@users.noreply.github.com>
2025-08-02 13:25:08 +0530

    add check to prevent duplicate short URLs

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -8,6 +8,12 @@ export async function POST(request) {
     const db = client.db("trimurl");
     const collection = db.collection("url");
 
+    // Check if shorturl already exists
+    const shortUrlExists = await collection.findOne({shorturl:body.shorturl});
+    if(shortUrlExists){
+        return Response.json({success:false, error:true, message:"Short URL already exists"});
+    }
+
 
     const result = await collection.insertOne({
         url: body.url,
